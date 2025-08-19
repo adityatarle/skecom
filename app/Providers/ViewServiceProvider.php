08a6a2f0b125f67b220316b1d17c367d8b70b789
Aditya Tarle <tarleaadii@gmail.com>
2025-08-19 11:04:11 +0530

    WIP: local changes in ViewServiceProvider

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-// Add these 'use' statements at the top
 use App\Models\ProductCategory;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -22,13 +21,12 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Using a closure based composer...
-        // This tells Laravel to run this function every time the 'layout.header' view is rendered.
         View::composer('layout.header', function ($view) {
+            // The query is now simplified.
+            // 1. Get all parent categories that are active.
+            // 2. For each of those, simply load all of their subcategories.
             $categories = ProductCategory::where('is_active', 1)
-                                         ->with(['subcategories' => function ($query) {
-                                             $query->where('is_active', 1); // Also get only active subcategories
-                                         }])
+                                         ->with('subcategories') // <-- MODIFIED: We removed the extra filter condition
                                          ->orderBy('name', 'asc')
                                          ->get();
                                          
